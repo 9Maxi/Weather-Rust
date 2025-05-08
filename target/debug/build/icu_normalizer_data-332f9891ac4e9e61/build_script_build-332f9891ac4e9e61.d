77fb4a6572975e2468c4045774c7ef3af3d0637d
9Maxi <146888507+9Maxi@users.noreply.github.com>
2025-05-08 19:53:06 -0300

    first commit

--- target/debug/build/icu_normalizer_data-332f9891ac4e9e61/build_script_build-332f9891ac4e9e61.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Master\Desktop\ruust\weather_rust\weather\target\debug\build\icu_normalizer_data-332f9891ac4e9e61\build_script_build-332f9891ac4e9e61.exe: C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.0.0\build.rs
-
-C:\Users\Master\Desktop\ruust\weather_rust\weather\target\debug\build\icu_normalizer_data-332f9891ac4e9e61\build_script_build-332f9891ac4e9e61.d: C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.0.0\build.rs
-
-C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\icu_normalizer_data-2.0.0\build.rs:
--- target/debug/deps/stable_deref_trait-06d9fe82067ff190.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Master\Desktop\ruust\weather_rust\weather\target\debug\deps\libstable_deref_trait-06d9fe82067ff190.rmeta: C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\stable_deref_trait-1.2.0\src\lib.rs
-
-C:\Users\Master\Desktop\ruust\weather_rust\weather\target\debug\deps\libstable_deref_trait-06d9fe82067ff190.rlib: C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\stable_deref_trait-1.2.0\src\lib.rs
-
-C:\Users\Master\Desktop\ruust\weather_rust\weather\target\debug\deps\stable_deref_trait-06d9fe82067ff190.d: C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\stable_deref_trait-1.2.0\src\lib.rs
-
-C:\Users\Master\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\stable_deref_trait-1.2.0\src\lib.rs:
